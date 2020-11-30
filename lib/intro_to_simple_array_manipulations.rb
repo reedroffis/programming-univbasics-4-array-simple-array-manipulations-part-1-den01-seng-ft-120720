534210a0a595e1c418ecabd8e45c642dfc85855e
Reed Roffis <reedroffis18@gmail.com>
2020-11-30 01:32:03 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+require "pry"
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end 
+
+def using_pop(array)
+  element = array.pop
+end
+
+def pop_with_args(array)
+binding.pry
+  array.pop
+end
