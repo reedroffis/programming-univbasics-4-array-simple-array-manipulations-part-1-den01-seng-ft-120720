4c8f98ff80fe0a2cd627ed679bed22fcf20f9ce7
Reed Roffis <reedroffis18@gmail.com>
2020-11-30 01:53:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,11 +8,11 @@ def using_unshift(array, string)
 end 
 
 def using_pop(array)
-  element = array.pop
+  array.pop
 end
 
 def pop_with_args(array)
- element = array.pop(2) 
+  array.pop(2) 
 end
 
 def using_shift(array)
